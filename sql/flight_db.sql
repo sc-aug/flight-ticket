7d1dbd0de99b9472a9afd0101355b86c2002a6f4
sc-aug <shichuanwang@yahoo.com>
2018-05-22 20:00:51 -0500

    db

---
+++ sql/flight_db.sql
@@ -0,0 +1,127 @@
+DROP DATABASE IF EXISTS flight_db;
+
+CREATE DATABASE flight_db;
+
+\connect flight_db
+
+-- role table - ok
+CREATE TABLE role (
+    role_id SERIAL NOT NULL,
+    role_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (role_id)
+);
+
+-- login account table - ok
+CREATE TABLE account (
+    uid SERIAL NOT NULL,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (role_id),
+    PRIMARY KEY (uid)
+);
+
+-- passenger info table - ok
+CREATE TABLE passenger (
+    pid SERIAL NOT NULL,
+    uid INT NOT NULL UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    ssn VARCHAR(10) NOT NULL CHECK (LENGTH(ssn) = 9),
+    age INT NOT NULL,
+    street VARCHAR(50) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    zip INT NOT NULL,
+    phone VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    FOREIGN KEY (uid) REFERENCES account (uid),
+    PRIMARY KEY (pid)
+);
+
+-- airplane info - ok
+CREATE TABLE airplane (
+    airplane_id SERIAL NOT NULL,
+    airplane_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (airplane_id)
+);
+
+-- seat type table - ok
+CREATE TABLE seat_type (
+    seat_type_id SERIAL NOT NULL,
+    seat_type_name VARCHAR(50) NOT NULL CHECK (LENGTH(seat_type_name) > 0),
+    PRIMARY KEY (seat_type_id)
+);
+
+-- airplane seat table - ok
+CREATE TABLE airplane_seat (
+    airplane_id INT NOT NULL,
+    seat_type_id INT NOT NULL,
+    seat_num INT NOT NULL,
+    FOREIGN KEY (airplane_id) REFERENCES airplane (airplane_id),
+    FOREIGN KEY (seat_type_id) REFERENCES seat_type (seat_type_id),
+    PRIMARY KEY (airplane_id, seat_type_id)
+);
+
+-- flight table - ok
+CREATE TABLE flight (
+    flight_id SERIAL NOT NULL,
+    relative_departure_time TIMESTAMP NOT NULL,
+    relative_arrival_time TIMESTAMP NOT NULL,
+    departure_city VARCHAR(50) NOT NULL,
+    arrival_city VARCHAR(50) NOT NULL,
+    airplane_id INT NOT NULL,
+    FOREIGN KEY (airplane_id) REFERENCES airplane (airplane_id),
+    PRIMARY KEY (flight_id)
+);
+
+-- schedule of flight table - ok
+CREATE TABLE schedule (
+    schedule_id SERIAL NOT NULL,
+    flight_id INT NOT NULL,
+    departure_time TIMESTAMP NOT NULL,
+    arrival_time TIMESTAMP NOT NULL,
+    departure_city VARCHAR(50) NOT NULL,
+    arrival_city VARCHAR(50) NOT NULL,
+    FOREIGN KEY (flight_id) REFERENCES flight (flight_id),
+    PRIMARY KEY (schedule_id)
+);
+
+-- reserved seats - ok
+CREATE TABLE reserved_seat (
+    schedule_id INT NOT NULL,
+    seat_type_id INT NOT NULL,
+    available_seat_num INT NOT NULL,
+    FOREIGN KEY (schedule_id) REFERENCES schedule (schedule_id),
+    FOREIGN KEY (seat_type_id) REFERENCES seat_type (seat_type_id),
+    PRIMARY KEY (schedule_id, seat_type_id)
+);
+
+-- price table - ok
+CREATE TABLE flight_price (
+    schedule_id INT NOT NULL UNIQUE,
+    price NUMERIC(10, 2),
+    FOREIGN KEY (schedule_id) REFERENCES schedule (schedule_id)
+);
+
+-- booking table - ok
+CREATE TABLE booking (
+    booking_id SERIAL NOT NULL,
+    pid INT NOT NULL,
+    schedule_id INT NOT NULL,
+    baggage VARCHAR(50),
+    seat_num INT NOT NULL,
+    class_id INT NOT NULL,
+    FOREIGN KEY (pid) REFERENCES passenger (pid),
+    FOREIGN KEY (schedule_id) REFERENCES schedule (schedule_id),
+    PRIMARY KEY (booking_id)
+);
+
+-- role
+INSERT INTO role VALUES (DEFAULT, 'admin');
+INSERT INTO role VALUES (DEFAULT, 'customer');
+
+-- account
+INSERT INTO account VALUES (DEFAULT, 'admin', 'admin', 1);
+INSERT INTO account VALUES (DEFAULT, 'demo', 'demo', 2);
+INSERT INTO account VALUES (DEFAULT, 'customer', 'customer', 2);
